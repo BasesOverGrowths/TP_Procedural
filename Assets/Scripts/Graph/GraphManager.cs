3ead5e482cb4652ac5328019f002900b3b98add9
Leonis26 <leonis2616@gmail.com>
2020-01-13 19:30:40 +0100

    New graph, compact

--- Assets/Scripts/Graph/GraphManager.cs
+++ Assets/Scripts/Graph/GraphManager.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Node
+{
+    public string name;
+    public Vector2Int location = Vector2Int.zero;
+    public LinkedList<Edge> adjacents = new LinkedList<Edge>();
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+}
 public class Edge
 {
     public Node node0;
