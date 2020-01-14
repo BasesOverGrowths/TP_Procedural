ca51cb34dc01907b46f59b5f2c2643cd4bfd024f
Leonis26 <leonis2616@gmail.com>
2020-01-14 12:38:04 +0100

    Wip secondary paths

--- Assets/Scripts/Graph/GraphManager.cs
+++ Assets/Scripts/Graph/GraphManager.cs
@@ -17,43 +17,71 @@ public class Edge
 {
     public Node node0;
     public Node node1;
-    public bool isOpen = true;
+    public Door.STATE doorState = Door.STATE.OPEN;
+    public Door door = null;
 
-    public Edge(Node n1, Node n2, bool open = true)
+    public Edge(Node n1, Node n2,  Door.STATE state = Door.STATE.OPEN)
     {
         node0 = n1;
         node1 = n2;
-        isOpen = open;
+        doorState = state;
     }
 }
 
 public class GraphManager : MonoBehaviour
 {
-    public int primaryPathSize = 3;
+    public const int primaryPathSize = 5;
 
     List<Node> adj = new List<Node>();
 
     Vector2Int[] directions = new Vector2Int[] { Vector2Int.up, Vector2Int.right, Vector2Int.left, Vector2Int.down };
     List<Vector2Int> allLocations = new List<Vector2Int>() { Vector2Int.zero };
 
-
+    List<int> currentSecPathPoss = new List<int>();
     void Awake(){
-        InitGraph();
+        InitGraph(); // graph main path
+
+        for (int i = 1; i < primaryPathSize; i++)
+        {
+            currentSecPathPoss.Add(i);
+        }
+
+        CreatePathAt(randomAvailableBranch(), Random.Range(2, primaryPathSize - 2), true);
+        CreatePathAt(randomAvailableBranch(), Random.Range(2, primaryPathSize - 2), true);
+
+        PrintGraph();
+    }
+
+    void CreatePathAt(int adjIndex, int length = primaryPathSize, bool isSecondary = false)
+    {
+        if (isSecondary)
+        {
+            adj[adjIndex].adjacents.Last.Value.doorState = Door.STATE.CLOSED; // closes the door to the next main path room
+            CreateNodeToNode(adj[adjIndex], "Sec_Generic0");
+        }
+
+
+        for (int i = 0; i < length; i++)
+        {
+            var nodeName = "Generic" + i;
+            if (isSecondary) nodeName = "Sec_" + nodeName;
+            CreateNodeToNode(adj[adjIndex + i], nodeName);
+        }
+    }
+    int randomAvailableBranch()
+    {
+        var randSubPathStart = currentSecPathPoss[Random.Range(0, currentSecPathPoss.Count)];
+        currentSecPathPoss.Remove(randSubPathStart);
+        return randSubPathStart;
     }
 
     void InitGraph()
     {
         adj.Add(new Node("Start"));
 
-        for (int i = 0; i < primaryPathSize; i++)
-        {
-            var nodeName = "Generic" + i;
-            CreateNodeToNode(adj[adj.Count - 1], nodeName);
-        }
+        CreatePathAt(adj.Count - 1);
 
         CreateNodeToNode(adj[adj.Count - 1], "End");
-
-        PrintGraph();
     }
 
     void PrintGraph()
@@ -64,18 +92,23 @@ public class GraphManager : MonoBehaviour
             foreach(Edge edge in node.adjacents)
             {
                 toLog += edge.node1.name;
-                if (!edge.isOpen) toLog += "(locked)";
+                switch (edge.doorState){
+                    case Door.STATE.CLOSED: toLog += "(locked)";
+                        break;
+                    case Door.STATE.SECRET: toLog += "(secret)";
+                        break;
+                }
                 toLog += " ";
             }
             Debug.Log(toLog);
         }
     }
 
-    void CreateNodeToNode(Node fromNode, string nodeName, bool open = true)
+    void CreateNodeToNode(Node fromNode, string nodeName, Door.STATE state = Door.STATE.OPEN)
     {
         var node = new Node(nodeName);
         InitNodeFrom(fromNode, node);
-        ConnectNodes(fromNode, node, open);
+        ConnectNodes(fromNode, node, state);
         adj.Add(node);
     }
 
@@ -97,9 +130,9 @@ public class GraphManager : MonoBehaviour
         return poss;
     }
 
-    public void ConnectNodes(Node A, Node B, bool open = true)
+    public void ConnectNodes(Node A, Node B, Door.STATE state = Door.STATE.OPEN)
     {
-        A.adjacents.AddLast(new Edge(A, B, open));
-        B.adjacents.AddLast(new Edge(B, A, open));
+        A.adjacents.AddLast(new Edge(A, B, state));
+        B.adjacents.AddLast(new Edge(B, A, state));
     }
 }
